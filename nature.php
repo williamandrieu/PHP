f8b3f3ae9e3caf535db1f9f153334ccb496b8530
William <wulloase95@gmail.com>
2019-02-18 11:32:21 +0100

    inutile

---
+++ nature.php
@@ -0,0 +1,68 @@
+<?php
+
+
+class Etre_Vivants 
+{
+
+	dureeDeVie
+	poids
+	taille
+
+	
+	function __construct(dureeDeVie,poids,taille)
+	{
+		$this->dureeDeVie = dureeDeVie;
+		$this->poids = poids;
+		$this->taille = taille;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
